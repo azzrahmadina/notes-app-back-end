7bfc325dc471c5c0c861a02e4f42f84f6b892ba3
rahmadina <azzrahmadina@gmail.com>
2024-11-23 17:30:02 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+/* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+  console.log(server.table());
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
